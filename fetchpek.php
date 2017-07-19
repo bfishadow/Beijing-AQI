0b0bcd487f0d0dfc1964341accf8de7087583c07
@bfishadow <julien.gong@gmail.com>
2017-07-19 15:11:57 +0800

    Added all data sources

--- fetchpek.php
+++ fetchpek.php
@@ -1,7 +1,15 @@
 <?php
 	do {
-		// Get data from us embassy in beijing
-		echo "Fetching data...<br />\n";
+		// Get data from US Embassy in Beijing
+		//
+		// All data sources are:
+		// http://www.stateair.net/web/rss/1/1.xml Beijing
+		// http://www.stateair.net/web/rss/1/2.xml Chengdu
+		// http://www.stateair.net/web/rss/1/3.xml Guangzhou
+		// http://www.stateair.net/web/rss/1/4.xml Shanghai
+		// http://www.stateair.net/web/rss/1/5.xml Shenyang
+
+		// echo "Fetching data...<br />\n";
 		$url = "http://www.stateair.net/web/rss/1/1.xml"; // Change the data source if you want
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -11,12 +19,13 @@
 		curl_close($ch);
 	} while (strlen($xml) < 1000);
 
-	echo "Data reday, ".strlen($xml)." bytes.<br />\n";
-	echo "Writing...<br />\n";
+	// echo "Data reday, ".strlen($xml)." bytes.<br />\n";
+	// echo "Writing...<br />\n";
+
 	// Write the data into local cache
 	$objFile = fopen('data.txt', 'w');
 	fwrite($objFile, $xml);
 	fclose($objFile);
 
-	echo "Done!<br />\n";
+	// echo "Done!<br />\n";
 ?>
